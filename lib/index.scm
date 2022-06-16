97bb67a6ba7389fbeef2313845f66cb1236c23a9
theblacksquid <theblacksquid@protonmail.com>
2022-06-17 03:15:22 +0800

    update readme and separate render commands

--- lib/index.scm
+++ lib/index.scm
@@ -4,4 +4,132 @@
 (load "lib/brp.scm")
 (load "lib/views.scm")
 (load "lib/controllers.scm")
-   
+
+(element-remove! ($ "#gear"))
+
+(render "#app"
+        (element-new app-template)
+        'html)
+
+(render "#profile-panel" 
+        (element-new profile-panel-template) 
+        'append)
+
+(let loop ((ls profile-items))
+	(if (null? ls)
+		'()
+		(begin
+			(render "#profile-panel" (profile-elem (car ls)) 'append)
+			(loop (cdr ls)))))
+
+(render "#skill-pt-panel"
+        (element-new
+          '(div
+            class "w3-container"
+            (p
+              id "skill-pt-label"
+            "Total Skill Points:  ")
+             (input 
+                type "text"
+                id "skillpoints"
+                class "w3-center w3-grey"
+                value "000"
+                disabled)))
+        'append)
+
+(render "#skill-pt-panel"
+        (element-new
+          '(div
+            id "skill-pt-btns"
+            class "w3-container"
+            ;style "display:block"
+            (button
+              id "add-skill-btn"
+              class "w3-btn"
+              ;style "display:inline-block"
+              "Add Skill")
+            (button
+              id "reset-skills-btn"
+              class "w3-btn"
+              ;style "display:inline-block"
+              "Reset Skill Points")))
+        'append)
+
+(let loop ((ls stats))
+  (if (null? ls)
+      '()
+      (begin
+        (render "#stats-panel" (make-stat-roller (car ls)) 'append)
+        (loop (cdr ls))
+      )))
+
+(render "#stats-panel" 
+        (element-new
+          '(div
+            class "w3-container"
+            style "padding: 0.35em 16px;"
+            (button
+              id "randomize"
+              ;style "width: 80%"
+              class "w3-btn-block"
+              "RANDOMIZE!!!"))) 
+        'append)
+
+(let loop ((ls derived-stats))
+  (if (null? ls)
+      '()
+      (begin
+        (render "#derived-stats-panel" 
+                (make-derived-stat-box (car ls)) 
+                'append)
+        (loop (cdr ls))
+      )))
+
+(let loop ((ls skill-list))
+	(if (null? ls)
+	    '()
+	    (begin
+	      (render "#skills-panel" 
+	              (skill-box (car (car ls)) (cadr (car ls)))
+	              'append)
+	      (-> ($ (string-append "#" (sanitize-name (car (car ls))) "-add")) 
+	          'click
+	          (js-closure 
+	            (lambda ()
+	              (skill-point-callback (car (car ls)) 'add))))
+	      (-> ($ (string-append "#" (sanitize-name (car (car ls))) "-subtract")) 
+	          'click
+	          (js-closure 
+	            (lambda ()
+	              (skill-point-callback (car (car ls)) 'subtract))))
+	      (loop (cdr ls))
+	    )))
+
+(render "#weapon-skills-panel" 
+        (weapon-skill-box "Brawl" 25 "1d4+db") 
+        'append)
+
+(-> ($ "#Brawl-add") 'click
+  (js-closure
+    (lambda ()
+      (skill-point-callback "Brawl" 'add))))
+      
+(-> ($ "#Brawl-subtract") 'click
+  (js-closure
+    (lambda ()
+      (skill-point-callback "Brawl" 'subtract))))
+
+(render "#weapon-skills-panel" 
+        (weapon-skill-box "Grapple" 25 "1d4+db") 
+        'append)
+
+(-> ($ "#Grapple-add") 'click
+  (js-closure
+    (lambda ()
+      (skill-point-callback "Grapple" 'add))))
+
+(-> ($ "#Grapple-subtract") 'click
+  (js-closure
+    (lambda ()
+      (skill-point-callback "Grapple" 'subtract))))
+
--- lib/views.scm
+++ lib/views.scm
@@ -150,203 +150,90 @@
         (p ,dmg-val)
         ))))
 
-(element-remove! ($ "#gear"))
+(define app-template
+  `(div style "margin:auto;"
+	(div id "main-navbar"
+	     style "display:block"
+	     
+	     (h1 "BRP Character Generator")
+	     
+	     (button id "character-panel-btn"
+		     class "w3-btn"
+		     "Character Profile")
+	     
+	     (button id "character-list-btn"
+		     class "w3-btn"
+		     "Character List")
+	     
+	     (button id "settings-panel-btn"
+		     class "w3-btn"
+		     "Settings"))
+	
+	(div id "character-panel"
+	     style "display:block"
+	     
+	     (div id "char-navbar"
+		  
+		  (button id "profile-btn"
+			  class "w3-btn-block"
+			  "Profile Overview")
+		  
+		  (button id "stats-btn"
+			  class "w3-btn-block"
+			  "Stats Overview")
+		  
+		  (button id "skills-btn"
+			  class "w3-btn-block"
+			  "Skills Overview"))
+	     
+	     (div class "w3-container"
+		  id "profile-info"
+		  
+		  (div id "profile-panel")
+		  
+		  (div id "char-results"
+		   (textarea id "charsheet"
+			     class "w3-grey"
+			     placeholder
+			     ,(string-append
+			       "Character Info goes here when "
+			       "you click 'Character Sheet'"
+			       "Or get the codes for your "
+			       "character by clicking on 'Get Codes'."))
+		   (div
+		    (button id "Clear"
+			    class "w3-btn"
+			    "Clear")
+		    
+		    (button id "Print"
+			    class "w3-btn"
+			    "Print"))))
+	     (div id "stats-overview"
+	      (div id "stats-panel")
+	      (div id "derived-stats-panel")
+	      (div id "stats-container"))
+	     
+	     (div id "skills-overview"
+		  class "w3-container"
+		  style ""
+	      (div id "skill-pt-panel")
+	      (div id "skills-panel")
+	      (div id "weapon-skills-panel")))))
 
-(render "#app"
-        (element-new
-          `(div
-            style "margin:auto;"
-            (div
-              id "main-navbar"
-              style "display:block"
-              (h1 "BRP Character Generator")
-              (button
-                id "character-panel-btn"
-                class "w3-btn"
-                "Character Profile")
-              (button
-                id "character-list-btn"
-                class "w3-btn"
-                "Character List")
-              (button 
-                id "settings-panel-btn"
-                class "w3-btn"
-                "Settings"))
-             (div
-              id "character-panel"
-              style "display:block"
-              (div
-                id "char-navbar"
-                (button
-                  id "profile-btn"
-                  class "w3-btn-block"
-                  "Profile Overview")
-                (button
-                  id "stats-btn"
-                  class "w3-btn-block"
-                  "Stats Overview")
-                (button
-                  id "skills-btn"
-                  class "w3-btn-block"
-                  "Skills Overview"))
-              (div
-                class "w3-container"
-                id "profile-info"
-                (div
-                  id "profile-panel")
-                  (div
-                    id "char-results"
-                    (textarea 
-                      id "charsheet"
-                      class "w3-grey"
-                      placeholder "Character Info goes here when you click 'Character Sheet'.
-                      Or get the codes for your character by clicking on 'Get Codes'.")
-                    (div
-                      (button
-                        id "Clear"
-                        class "w3-btn"
-                        "Clear") 
-                      (button
-                        id "Print"
-                        class "w3-btn"
-                        "Print"))
-                  )
-               )
-               (div 
-                 id "stats-overview"
-                (div 
-                  id "stats-panel"
-                  )
-                (div 
-                  id "derived-stats-panel"
-                  )
-                (div
-                  id "stats-container" 
-                 )
-               )
-               (div
-                  id "skills-overview"
-                  class "w3-container"
-                  style ""
-                  (div 
-                    id "skill-pt-panel")
-                  (div 
-                    id "skills-panel")
-                  (div
-                    id "weapon-skills-panel")
-               )
-             )) 
-           )
-        'html)
-
-(let loop ((ls profile-items))
-	(if (null? ls)
-		'()
-		(begin
-			(render "#profile-panel" (profile-elem (car ls)) 'append)
-			(loop (cdr ls))
-		)))
-
-(render "#profile-panel" 
-        (element-new
-          '(div
-              id "profile-btn-panel"
-            (button
-              id "save-character"
-              class "w3-btn"
-              "Save Character")
-             (button
-              id "get-character-sheet"
-              class "w3-btn"
-              "Character Sheet")
-             (button
-              id "get-codes"
-              class "w3-btn"
-              "Get Codes"))) 
-        'append)
-
-(render "#skill-pt-panel"
-        (element-new
-          '(div
-            class "w3-container"
-            (p
-              id "skill-pt-label"
-            "Total Skill Points:  ")
-             (input 
-                type "text"
-                id "skillpoints"
-                class "w3-center w3-grey"
-                value "000"
-                disabled)))
-        'append)
-
-(render "#skill-pt-panel"
-        (element-new
-          '(div
-            id "skill-pt-btns"
-            class "w3-container"
-            ;style "display:block"
-            (button
-              id "add-skill-btn"
-              class "w3-btn"
-              ;style "display:inline-block"
-              "Add Skill")
-            (button
-              id "reset-skills-btn"
-              class "w3-btn"
-              ;style "display:inline-block"
-              "Reset Skill Points")))
-        'append)
-
-(let loop ((ls stats))
-  (if (null? ls)
-      '()
-      (begin
-        (render "#stats-panel" (make-stat-roller (car ls)) 'append)
-        (loop (cdr ls))
-      )))
-
-(render "#stats-panel" 
-        (element-new
-          '(div
-            class "w3-container"
-            style "padding: 0.35em 16px;"
-            (button
-              id "randomize"
-              ;style "width: 80%"
-              class "w3-btn-block"
-              "RANDOMIZE!!!"))) 
-        'append)
-
-(let loop ((ls derived-stats))
-  (if (null? ls)
-      '()
-      (begin
-        (render "#derived-stats-panel" 
-                (make-derived-stat-box (car ls)) 
-                'append)
-        (loop (cdr ls))
-      )))
-
-(let loop ((ls skill-list))
-	(if (null? ls)
-	    '()
-	    (begin
-	      (render "#skills-panel" 
-	              (skill-box (car (car ls)) (cadr (car ls)))
-	              'append)
-	      (-> ($ (string-append "#" (sanitize-name (car (car ls))) "-add")) 
-	          'click
-	          (js-closure 
-	            (lambda ()
-	              (skill-point-callback (car (car ls)) 'add))))
-	      (-> ($ (string-append "#" (sanitize-name (car (car ls))) "-subtract")) 
-	          'click
-	          (js-closure 
-	            (lambda ()
-	              (skill-point-callback (car (car ls)) 'subtract))))
-	      (loop (cdr ls))
-	    )))
+(define profile-panel-template
+  '(div id "profile-btn-panel"
+	
+    (button id "save-character"
+	    class "w3-btn"
+	    "Save Character")
+    
+    (button id "get-character-sheet"
+	    class "w3-btn"
+	    "Character Sheet")
+    
+    (button id "get-codes"
+	    class "w3-btn"
+	    "Get Codes")))
 
 (define create-list-elem
   (lambda (ls)
@@ -362,35 +249,6 @@
                            "</li>"
                            (loop (cdr lst)))))
       "</ul>")))
-
-(render "#weapon-skills-panel" 
-        (weapon-skill-box "Brawl" 25 "1d4+db") 
-        'append)
-
-(-> ($ "#Brawl-add") 'click
-  (js-closure
-    (lambda ()
-      (skill-point-callback "Brawl" 'add))))
-      
-(-> ($ "#Brawl-subtract") 'click
-  (js-closure
-    (lambda ()
-      (skill-point-callback "Brawl" 'subtract))))
-
-(render "#weapon-skills-panel" 
-        (weapon-skill-box "Grapple" 25 "1d4+db") 
-        'append)
-
-(-> ($ "#Grapple-add") 'click
-  (js-closure
-    (lambda ()
-      (skill-point-callback "Grapple" 'add))))
-
-(-> ($ "#Grapple-subtract") 'click
-  (js-closure
-    (lambda ()
-      (skill-point-callback "Grapple" 'subtract))))
-
 
 (define add-skill-box
   (element-new 
